203d2f76c5467c89836bccdb5e5c02798ed7978f
Lucia Vazquez <131535537+LuciaVazquez08@users.noreply.github.com>
2024-03-12 10:38:32 -0300

    clase 2

--- Clase2-ParadigmaFuncional/ordenSuperior.py
+++ Clase2-ParadigmaFuncional/ordenSuperior.py
@@ -5,10 +5,11 @@ def wrapper(fn: Callable[[None],None], *args, **kwargs):
     print("Ejecutada "+fn.__name__)
 
 def ejemplo():
-    print("Eejcucion de la funcion de prueba")
+    print("Ejcucion de la funcion de prueba")
 
 def ejemplo2(mensaje):
     print(mensaje)
 
 wrapper(ejemplo)
-wrapper(ejemplo2,"ejecusion con argumentos")
+print("")
+wrapper(ejemplo2,"ejecutada con argumentos")
