f508dd2708133ae49165e7de0f642a7fecb534c7
Lucia <luchi08vazquez@gmail.com>
2024-03-19 15:13:17 -0300

    recursion de cola

--- Clase4-PilayCola/desde_hasta.py
+++ Clase4-PilayCola/desde_hasta.py
@@ -1,21 +1,29 @@
 def desde_hasta_no_inclusiva(x:int, y:int) -> list[int]:
     if y < x:
         raise ValueError("No es posible con ese orden de numeros")
+    acumulado =[]
     def desde_hasta_intrna(x,y,acumulado):
         if x == y:
             return acumulado
         else:
-            return desde_hasta_intrna(x+1,y,acumulado+x)
-    return desde_hasta_intrna(x,y,0)
+            acumulado.append(x)
+            return desde_hasta_intrna(x+1,y,acumulado)
+    return desde_hasta_intrna(x,y, acumulado)
 
 def desde_hasta_inclusiva(x:int, y:int) -> list[int]:
-    if y < x:
+    if y <= x:
         raise ValueError("No es posible con ese orden de numeros")
+    acumulado =[]
     def desde_hasta_intrna(x,y,acumulado):
         if x == y:
-            return acumulado + x
+            acumulado.append(x)
+            return acumulado
         else:
-            return desde_hasta_intrna(x+1,y,acumulado+x)
-    return desde_hasta_intrna(x,y,0)
+            acumulado.append(x)
+            return desde_hasta_intrna(x+1,y,acumulado)
+    return desde_hasta_intrna(x,y,acumulado)
 
-print(desde_hasta_no_inclusiva(0,3))   
+print(desde_hasta_inclusiva(0,3))   
+
+
+
