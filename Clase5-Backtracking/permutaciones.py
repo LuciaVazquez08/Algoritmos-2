06c630d0c76c3c1150cccffe1bbf8d13ef992728
Lucia Vazquez <131535537+LuciaVazquez08@users.noreply.github.com>
2024-03-26 12:13:11 -0300

    backtracking

---
+++ Clase5-Backtracking/permutaciones.py
@@ -0,0 +1,16 @@
+from math import factorial
+
+
+def permutar(lista_og: list[int]) -> list[list[int]]:    
+    permutaciones = []
+
+    def permutar_interna(perm, resto):
+        if not resto:
+            permutaciones.append(perm)
+        else: 
+            for i in range(len(resto)):
+                permutar_interna(perm + [resto[i]], resto[:i] + resto[i+1:])
+
+
+    permutar_interna([], lista_og)
+    return permutaciones
